3def412df4bf7a13f5655b3617ab73dcb5b29163
Sjors Provoost <sjors@sprovoost.nl>
2009-04-10 22:28:29 +0200

    Removed database.yml from repository.

--- config/environment.rb
+++ config/environment.rb
@@ -28,7 +28,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   
-  config.gem 'mysql'
+  config.gem "capistrano-ext", :lib => "capistrano"
+  #config.gem 'mysql'
 
   config.gem 'mislav-will_paginate', :version => '~> 2.2.3', :lib => 'will_paginate', :source => 'http://gems.github.com'
 
