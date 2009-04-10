2aab4599d5e6f0a09314cca4483c3ab20682b195
Sjors Provoost <sjors@sprovoost.nl>
2009-04-10 13:27:59 +0200

    Upgraded to rails 2.3.2. Changed import method.

--- import-stars.sql
+++ import-stars.sql
@@ -1,4 +1,5 @@
-load data local infile 'kepler.txt' into table stars
+load data local infile 'KIC.txt' into table stars
+IGNORE 16 LINES
 (@var1)
 SET number=SUBSTR(@var1,1,13),
 coordinate=SUBSTR(@var1,17,24),
@@ -7,3 +8,5 @@ teff=SUBSTR(@var1,54,4),
 logg=SUBSTR(@var1,61,5),
 mh=SUBSTR(@var1,69,5),
 adopted_by=SUBSTR(@var1,78,23);
+
+DELETE FROM stars WHERE number = "-------------";
