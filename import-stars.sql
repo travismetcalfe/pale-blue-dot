01658efc59ca45dc629a6b8c4563948b99f97743
Travis Metcalfe <travis@whitedwarf.org>
2019-06-05 16:05:46 -0600

    Uploaded the correct import script

--- import-stars.sql
+++ import-stars.sql
@@ -1,20 +1,20 @@
 load data local infile 'KIC.txt' REPLACE into table stars
 (@var1)
-SET id=SUBSTR(@var1, 4,10),
-number=SUBSTR(@var1,1,13),
-flag=SUBSTR(@var1,17,1),
-lon_d=SUBSTR(@var1,21,2),
-lon_m=SUBSTR(@var1,24,2),
-lon_s=SUBSTR(@var1,27,4),
-lat_d=SUBSTR(@var1,35,3),
-lat_m=SUBSTR(@var1,39,2),
-lat_s=SUBSTR(@var1,42,2),
-mag=SUBSTR(@var1,49,5),
-teff=SUBSTR(@var1,58,4),
-logg=SUBSTR(@var1,65,5),
-mh=SUBSTR(@var1,73,5),
-adopted_by=SUBSTR(@var1,82);
+SET id=SUBSTR(@var1, 5,10),
+number=SUBSTR(@var1,1,14),
+flag=SUBSTR(@var1,18,1),
+const=SUBSTR(@var1,22,3),
+lon_d=SUBSTR(@var1,28,2),
+lon_m=SUBSTR(@var1,31,2),
+lon_s=SUBSTR(@var1,34,4),
+lat_sign=SUBSTR(@var1,42,1),
+lat_d=SUBSTR(@var1,42,3),
+lat_m=SUBSTR(@var1,46,2),
+lat_s=SUBSTR(@var1,49,2),
+mag=SUBSTR(@var1,56,5),
+adopted_by=SUBSTR(@var1,65);
 
+DELETE FROM stars WHERE flag = "0";
 DELETE FROM stars WHERE number = " ";
 DELETE FROM stars WHERE number = "-------------";
 
