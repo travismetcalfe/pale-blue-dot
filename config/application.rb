a3a39e5327cf5122f9419e07cc05ff5dd4a63274
Travis Metcalfe <travis@whitedwarf.org>
2019-07-15 15:01:56 -0600

    allow view in iframe
    Allow application to load in iframe from main website

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module Testrails
     config.load_defaults 5.2
     config.public_file_server.enabled = true
     config.google_maps_api_key = "YOUR_API_KEY_HERE"
+    # Allow application to load in iframe from main website
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'https://adoptastar.org'
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
